3c138a57a30c9ebbf042d563cc5b9e1757721a48
marbey <contact@marbey.dev>
2024-11-05 08:18:10 +0100

    + mold_1_id ipv mold_1

--- Infrastructure/Repositories/MoldRepository.cs
+++ Infrastructure/Repositories/MoldRepository.cs
@@ -15,7 +15,7 @@ public class MoldRepository
     public async Task<Object> GetMoldHistoryAsync(int skip, int limit)
     {
         var moldHistory = await _connection.QueryAsync(
-            "SELECT timestamp(start_date, start_time) as start, timestamp(end_date, end_time) as end, treeview_id as mold_1, treeview2_id as mold_2, t1.naam as mold_1_name, t1.omschrijving as mold_1_description, t2.naam as mold_2_name, t2.omschrijving as mold_2_description FROM production_data LEFT JOIN treeview t1 ON t1.id = production_data.treeview_id LEFT JOIN treeview t2 ON t2.id = treeview2_id LIMIT @skip,@limit", new {skip, limit});
+            "SELECT timestamp(start_date, start_time) as start, timestamp(end_date, end_time) as end, treeview_id as mold_1_id, treeview2_id as mold_2_id, t1.naam as mold_1_name, t1.omschrijving as mold_1_description, t2.naam as mold_2_name, t2.omschrijving as mold_2_description FROM production_data LEFT JOIN treeview t1 ON t1.id = production_data.treeview_id LEFT JOIN treeview t2 ON t2.id = treeview2_id LIMIT @skip,@limit", new {skip, limit});
 
         return moldHistory;
     }
